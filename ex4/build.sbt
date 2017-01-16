7dc43f35ecc80658158f85e7ff722bd21c1ac129
Mukul Dev <devmukul44@gmail.com>
2017-01-16 13:09:21 +0530

    ex4 cli package added

---
+++ ex4/build.sbt
@@ -0,0 +1,6 @@
+organization := "com.innovaccer"
+
+scalaVersion := "2.11.7"
+
+version := "1.0-SNAPSHOT"
+
---
+++ ex4/src/main/scala/ex4.scala
@@ -0,0 +1,23 @@
+class InnoString(str : String){
+	private var name = str
+	override def toString = str
+	def concat(arg: String) = {
+		name = name + arg
+		println(this.name)
+	}
+	def +(that: InnoString) = new InnoString(this.name + that.name)
+	def ==(that: InnoString) = new InnoString((this.name == that.name).toString)
+	def isPalindrome() = {
+		this.name == this.name.reverse
+	}
+}
+object ex4 {
+	def main(args: Array[String]) :Unit = {
+		val instance = new InnoString("Inno")
+		instance.concat("Vaccer")
+		val instance2 = new InnoString("ABCBA")
+		println(instance + instance2)
+		println(instance == instance2)
+		println(instance2.isPalindrome)
+	}
+}
